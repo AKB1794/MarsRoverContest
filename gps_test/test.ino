1af1da5355f11f585d1bb621b4f80453101d82ea
akb1794 <solve.akb1794@gmail.com>
2022-01-29 11:43:55 +0600

    Gps testiong

---
+++ gps_test/test.ino
@@ -0,0 +1,33 @@
+#include "TinyGPS++.h"
+#include "SoftwareSerial.h"
+float lati;
+float longi;
+SoftwareSerial serial_connection(10, 11); //RX=pin 10, TX=pin 11
+TinyGPSPlus gps;//This is the GPS object that will pretty much do all the grunt work with the NMEA data
+void setup()
+{
+  Serial.begin(9600);//This opens up communications to the Serial monitor in the Arduino IDE
+  serial_connection.begin(9600);//This opens up communications to the GPS
+  Serial.println("GPS Start");//Just show to the monitor that the sketch has started
+}
+
+void loop()
+{
+  while(serial_connection.available())//While there are characters to come from the GPS
+  {
+    gps.encode(serial_connection.read());//This feeds the serial NMEA data into the library one char at a time
+  }
+  if(gps.location.isUpdated())//This will pretty much be fired all the time anyway but will at least reduce it to only after a package of NMEA data comes in
+  {
+    //Get the latest info from the gps object which it derived from the data sent by the GPS unit
+    
+    lati=(gps.location.lat());
+    longi=(gps.location.lng());
+    Serial.println(lati ,8);
+    Serial.println(longi ,8);
+    Serial.println();
+    delay(1000);
+  }
+}
+
+
