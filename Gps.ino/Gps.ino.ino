55ae400bbbbed8f927c74b9b7c5b5b6f3ba94eee
akb1794 <solve.akb1794@gmail.com>
2022-01-29 11:47:04 +0600

    GPS added

---
+++ Gps.ino/Gps.ino.ino
@@ -0,0 +1,40 @@
+#include <TinyGPS.h>
+#include <SoftwareSerial.h>
+
+TinyGPS gps;
+SoftwareSerial gpsSerial(10,11);
+
+long lat ,lon;
+
+void setup()
+{
+  Serial.begin(115200);
+  gpsSerial.begin(9600);
+  
+  delay (100);
+}
+
+
+void loop()
+{
+  while (gpsSerial.available())
+  {
+    int c = gpsSerial.read();
+    if (gps.encode(c))
+    {
+      gps.get_position(&lat, &lon);
+      Serial.print(lat/1000000);
+      Serial.print(".");
+      Serial.print(lat%1000000);
+      Serial.print("  ");
+      Serial.print(lon/1000000);
+      Serial.print(".");
+      Serial.print(lon%1000000);
+      Serial.print("  ");
+     
+    }
+    delay(1000);
+  }
+  
+}
+
